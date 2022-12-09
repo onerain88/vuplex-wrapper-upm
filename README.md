68aba3b2d4f5fcb956adaee759a758df600893e1
oneRain <onerain88@gmail.com>
2022-12-09 11:57:22 +0800

    init

---
+++ README.md
@@ -0,0 +1,1 @@
+# Vuplex Wrapper
---
+++ Script/VuplexWrapper.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Vuplex.WebView;
+
+public class VuplexWrapper : MonoBehaviour
+{
+    // Start is called before the first frame update
+    async void Start()
+    {
+        GameObject webviewGO = Instantiate(Resources.Load("CanvasWebViewPrefab")) as GameObject;
+        webviewGO.transform.SetParent(transform);
+        RectTransform rectTransform = webviewGO.GetComponent<RectTransform>();
+        rectTransform.offsetMin = Vector2.zero;
+        rectTransform.offsetMax = Vector2.zero;
+
+        CanvasWebViewPrefab webViewPrefab = webviewGO.GetComponent<CanvasWebViewPrefab>();
+        webViewPrefab.InitialUrl = "https://www.baidu.com";
+
+        await webViewPrefab.WaitUntilInitialized();
+    }
+}
